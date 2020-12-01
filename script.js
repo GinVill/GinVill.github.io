dca4f44efb94e8ec644a9b2d01bcf7b37c0e86f8
GinVill <ginayvillegas@gmail.com>
2020-11-30 21:42:56 -0800

    More Form files added

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault;
+  console.log("submit");
+});
+
+document.getElementById("destination").nodeValue;
+
+function myFunction() {
+  document.getElementById("Form1").onsubmit();
+}
